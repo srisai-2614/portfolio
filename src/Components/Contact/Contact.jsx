3755473981d8e7e769ca019f35e125a0618a8154
Pranathi Akula <pranathiakula@Pranathis-MacBook-Air.local>
2024-10-14 08:11:19 +0530

    icons updated

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -3,15 +3,15 @@ import './Contact.css';
 
 function Contact(props) {
   return (
-    <div style={{ padding: "1em" }}>
+    <div style={{ padding: "1em", borderRadius:'50%'}}>
       {props.link ? (
         <a href={props.link}>
-          <img src={props.image} alt={props.text} className="Image" />
+          <img src={props.image} alt={props.text} className="Image" style={{width:"3.2em", height:'3.4em'}}/>
         </a>
       ) : (
-        // If 'link' is not provided, open a mailto link
+      
         <a href={`mailto:pavansrisai2614@gmail.com`}>
-          <img src={props.image} alt={props.text} className="Image" />
+          <img src={props.image} alt={props.text} className="Image" style={{width:"3.2em", height:'3em'}}/>
         </a>
       )}
     </div>
